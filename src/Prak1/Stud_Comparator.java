a5514d88606cd0ad0b5813e542c8099c710cc9a0
qrave1 <liveartem@yandex.ru>
2023-02-12 17:16:49 +0300

    Init

--- src/Prak1/Stud_Comparator.java
+++ src/Prak1/Stud_Comparator.java
@@ -5,8 +5,6 @@ import java.util.Comparator;
 public class Stud_Comparator implements Comparator<Student> {
     @Override
     public int compare(Student s1, Student s2) {
-//        return Integer.compare(s1.getScore(), s2.getScore());
         return Math.max(s1.getScore(), s2.getScore());
     }
 }
-
